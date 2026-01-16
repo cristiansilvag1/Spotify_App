59e522a33dcf79cddf6f9d1f9d406f4a10f17487
cristiansilvag1 <christiangrazzian@gmail.com>
2026-01-16 18:47:15 -0500

    Initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,30 +11,33 @@ import { CommonModule } from '@angular/common';
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class HomePage {
+  // Colores de tema
   colorClaro = 'var(--color-claro)';
   colorOscuro = 'var(--color-oscuro)'; 
   colorActual = this.colorOscuro;
 
+  // Géneros musicales
   genres = [
     {
-      title: "music clasic",
+      title: "Music Classic",
       image: "https://static.vecteezy.com/system/resources/previews/003/335/968/large_2x/the-violin-on-table-classic-musical-instrument-used-in-the-orchestra-free-photo.jpg",
-      description: "Lorem ipsum es el texto que se usa habitualmente en diseño gráfico en demostraciones de tipografías o de bocetos para diseños para probar el arte visual antes de insertar el texto final."
+      
     },
     {
       title: "Afrobeats",
       image: "https://static.vecteezy.com/system/resources/previews/002/236/321/large_2x/electric-guitar-instrument-musical-isolated-icon-free-vector.jpg",
-      description: "Los mejores temas de afrobeats están aquí."
+      description: "Los mejores temas de Afrobeats están aquí, con ritmos que te harán mover el cuerpo y sentir la energía de cada canción."
     },
     {
       title: "Rock & Roll",
       image: "https://static.vecteezy.com/system/resources/previews/002/236/315/large_2x/saxophone-instrument-musical-isolated-icon-free-vector.jpg",
-      description: "Siente la energía de los mejores clásicos del rock."
+      description: "Siente la energía de los clásicos del Rock & Roll, con guitarras eléctricas, baterías potentes y letras que marcaron generaciones."
     }
   ];
 
   constructor() {}
 
+  // Cambiar tema claro/oscuro
   cambiarColor() {
     this.colorActual = (this.colorActual === this.colorOscuro) ? this.colorClaro : this.colorOscuro;
   }
